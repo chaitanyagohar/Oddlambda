fb538d6a4b0a7459021febb780d29cda80f1da00
chaitanyagohar <chaitanyagohar@gmail.com>
2026-09-05 21:39:16 +0530

    ad

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Projects from "./components/Projects";
 import About from "./components/About";
-import FAQ from "./components/Faq";
 import CTA from "./components/CTA";
 import Footer from "./components/Footer";
 import CookieConsent from "./components/CookieConsent";
